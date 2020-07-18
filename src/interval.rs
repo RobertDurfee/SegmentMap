904ba546b5b6d484cbb04b07d196cd3298ac9ba4
Robert Durfee <robert.durfee.17@gmail.com>
2020-07-18 06:45:04 -0500

    added convenience creators

--- src/interval.rs
+++ src/interval.rs
@@ -1,3 +1,10 @@
+use std::ops::Add;
+use num::{
+    Bounded,
+    Zero,
+    One,
+};
+
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Interval<K> {
     lower: K,
@@ -49,6 +56,66 @@ impl<K: Clone + PartialOrd> Interval<K> {
             lower: if self.lower < other.lower { self.lower.clone() } else { other.lower.clone() },
             upper: if other.upper < self.upper { self.upper.clone() } else { other.upper.clone() },
         }
+    }
+}
+
+impl<K: PartialOrd + Zero> Interval<K> {
+    pub fn empty() -> Interval<K> {
+        Interval { lower: K::zero(), upper: K::zero() }
+    }
+}
+
+impl<K: Add<Output = K> + Clone + One + PartialOrd> Interval<K> {
+    pub fn singleton(value: K) -> Interval<K> {
+        Interval { lower: value.clone(), upper: value + K::one() }
+    }
+}
+
+impl<K: Add<Output = K> + One + PartialOrd> Interval<K> {
+    pub fn open(lower: K, upper: K) -> Interval<K> {
+        Interval { lower: lower + K::one(), upper }
+    }
+
+    pub fn closed(lower: K, upper: K) -> Interval<K> {
+        Interval { lower, upper: upper + K::one() }
+    }
+
+    pub fn open_closed(lower: K, upper: K) -> Interval<K> {
+        Interval { lower: lower + K::one(), upper: upper + K::one() }
+    }
+}
+
+impl<K: PartialOrd> Interval<K> {
+    pub fn closed_open(lower: K, upper: K) -> Interval<K> {
+        Interval { lower, upper }
+    }
+}
+
+impl<K: Add<Output = K> + Bounded + One + PartialOrd> Interval<K> {
+    pub fn at_most(value: K) -> Interval<K> {
+        Interval { lower: K::min_value(), upper: value + K::one() }
+    }
+}
+
+impl<K: Bounded + PartialOrd> Interval<K> {
+    pub fn at_least(value: K) -> Interval<K> {
+        Interval { lower: value, upper: K::max_value() }
+    }
+
+    pub fn less_than(value: K) -> Interval<K> {
+        Interval { lower: K::min_value(), upper: value }
+    }
+}
+
+impl<K: Add<Output = K> + Bounded + One + PartialOrd> Interval<K> {
+    pub fn greater_than(value: K) -> Interval<K> {
+        Interval { lower: value + K::one(), upper: K::max_value() }
+    }
+}
+
+impl<K: Bounded + PartialOrd> Interval<K> {
+    pub fn all() -> Interval<K> {
+        Interval { lower: K::min_value(), upper: K::max_value() }
     }
 }
 

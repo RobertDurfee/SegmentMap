31a0428430bc40dde8d3e84467b15d9e1d870a3d
Robert Durfee <robert.durfee.17@gmail.com>
2020-08-22 21:38:54 -0500

    export missing iterator structs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,15 @@ mod segment_map;
 mod bounded;
 mod next;
 
-pub use crate::segment_map::SegmentMap;
+pub use crate::segment_map::{
+    SegmentMap,
+    Segments,
+    Values,
+    ValuesMut,
+    Iter,
+    IterMut,
+    IntoIter,
+};
 pub use crate::segment::Segment;
 pub use crate::bounded::Bounded;
 pub use crate::next::Next;
